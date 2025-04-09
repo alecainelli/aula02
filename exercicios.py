6fac4e0aa269524f93d3d32f9e606200f1fd7979
Alexandre Cainelli <alecainelli@gmail.com>
2025-04-09 19:08:33 -0300

    exercicio 2

---
+++ exercicios.py
@@ -0,0 +1,9 @@
+# 1
+# num1 = int(input("Digite o primeiro número: "))
+# num2 = int(input("Digite o segundo número: "))
+# print(num1+num2)
+
+# 2
+num1 = int(input("Digite um número: "))
+calc = num1 % 5
+print("O resto da divisão por 5 é: ", calc)
