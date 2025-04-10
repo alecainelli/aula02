71d1e3a653baf2d74702b7522d7d5fc1e482dff5
Alexandre Cainelli <alecainelli@gmail.com>
2025-04-10 15:43:55 -0300

    exercicio 8

--- exercicios.py
+++ exercicios.py
@@ -9,9 +9,46 @@
 #print("O resto da divisão por 5 é: ", calc)
 
 #3
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-#num1 = 5  # Exemplo de entrada
-#num2 = 7  # Exemplo de entrada
-resultado_multiplicacao = num1 * num2
-print("O resultado da multiplicação é:", resultado_multiplicacao)
+#num1 = int(input("Digite o primeiro número: "))
+#num2 = int(input("Digite o segundo número: "))
+#resultado_multiplicacao = num1 * num2
+#print("O resultado da multiplicação é:", resultado_multiplicacao)
+
+#4
+# num1 = int(input("Digite o primeiro número inteiro: "))
+# num2 = int(input("Digite o segundo número inteiro: "))
+#num1 = 20  # Exemplo de entrada
+#num2 = 3  # Exemplo de entrada
+#resultado_divisao_inteira = num1 // num2
+#print("O resultado da divisão inteira é:", resultado_divisao_inteira)
+
+#5
+# num = int(input("Digite um número: "))
+#num = 6  # Exemplo de entrada
+#resultado_quadrado = num ** 2
+#print("O quadrado do número é:", resultado_quadrado)
+
+#6
+#num1 = float(input("Digite o primeiro número flutuante: "))
+#num2 = float(input("Digite o segundo número flutuante: "))
+#print("A soma é: ", num1 + num2)
+
+#7
+# num1 = float(input("Digite o primeiro número flutuante: "))
+# num2 = float(input("Digite o segundo número flutuante: "))
+#num1 = 3.5  # Exemplo de entrada
+#num2 = 7.5  # Exemplo de entrada
+#media = (num1 + num2) / 2
+#print("A média é:", media)
+
+#8
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+#base = 2.0  # Exemplo de entrada
+#expoente = 3.0  # Exemplo de entrada
+potencia = base ** expoente
+print("O resultado da potência é:", potencia)
+
+#9
+
+#10
